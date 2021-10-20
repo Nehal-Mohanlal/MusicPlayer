7d3d317d7ae29f098769e22a40b6c1880d0dabcc
Nehal <nehal.mohanlal12@gmail.com>
2021-10-20 17:12:25 +0200

    Add project files.

---
+++ MusicPlayer/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿
+namespace MusicPlayer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.topPanel = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.listboxSongs = new System.Windows.Forms.ListBox();
+            this.btnSelectSongs = new System.Windows.Forms.Button();
+            this.WMPmusic = new AxWMPLib.AxWindowsMediaPlayer();
+            this.label2 = new System.Windows.Forms.Label();
+            this.topPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.WMPmusic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // topPanel
+            // 
+            this.topPanel.BackColor = System.Drawing.Color.Black;
+            this.topPanel.Controls.Add(this.pictureBox1);
+            this.topPanel.Controls.Add(this.label1);
+            this.topPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.topPanel.Location = new System.Drawing.Point(0, 0);
+            this.topPanel.Name = "topPanel";
+            this.topPanel.Size = new System.Drawing.Size(807, 64);
+            this.topPanel.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.label1.Location = new System.Drawing.Point(37, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Whats Playing? ";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(793, 51);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(11, 10);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // listboxSongs
+            // 
+            this.listboxSongs.BackColor = System.Drawing.Color.Black;
+            this.listboxSongs.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listboxSongs.ForeColor = System.Drawing.Color.LightGray;
+            this.listboxSongs.FormattingEnabled = true;
+            this.listboxSongs.ItemHeight = 16;
+            this.listboxSongs.Location = new System.Drawing.Point(588, 144);
+            this.listboxSongs.Name = "listboxSongs";
+            this.listboxSongs.Size = new System.Drawing.Size(200, 260);
+            this.listboxSongs.TabIndex = 1;
+            this.listboxSongs.SelectedIndexChanged += new System.EventHandler(this.listboxSongs_SelectedIndexChanged);
+            // 
+            // btnSelectSongs
+            // 
+            this.btnSelectSongs.BackColor = System.Drawing.Color.DarkCyan;
+            this.btnSelectSongs.Font = new System.Drawing.Font("Segoe UI Light", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSelectSongs.Location = new System.Drawing.Point(588, 85);
+            this.btnSelectSongs.Name = "btnSelectSongs";
+            this.btnSelectSongs.Size = new System.Drawing.Size(200, 50);
+            this.btnSelectSongs.TabIndex = 2;
+            this.btnSelectSongs.Text = "Add to Queue";
+            this.btnSelectSongs.UseVisualStyleBackColor = false;
+            this.btnSelectSongs.Click += new System.EventHandler(this.btnSelectSongs_Click);
+            // 
+            // WMPmusic
+            // 
+            this.WMPmusic.Enabled = true;
+            this.WMPmusic.Location = new System.Drawing.Point(12, 85);
+            this.WMPmusic.Name = "WMPmusic";
+            this.WMPmusic.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("WMPmusic.OcxState")));
+            this.WMPmusic.Size = new System.Drawing.Size(560, 319);
+            this.WMPmusic.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(585, 432);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Done By Nehal Mohanlal";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(807, 454);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.WMPmusic);
+            this.Controls.Add(this.btnSelectSongs);
+            this.Controls.Add(this.listboxSongs);
+            this.Controls.Add(this.topPanel);
+            this.ForeColor = System.Drawing.Color.White;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.topPanel.ResumeLayout(false);
+            this.topPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.WMPmusic)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel topPanel;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listboxSongs;
+        private System.Windows.Forms.Button btnSelectSongs;
+        private AxWMPLib.AxWindowsMediaPlayer WMPmusic;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ MusicPlayer/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MusicPlayer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // Global arrays to save song names
+
+        string[] Songs, paths;
+
+        private void btnSelectSongs_Click(object sender, EventArgs e)
+        {
+            //code to select the songs 
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            ofd.Multiselect = true; 
+
+                if(ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                Songs = ofd.SafeFileNames; // save name of track into Song array
+                paths = ofd.FileNames; // save path of track in path Array
+                // Display music titles in for loop  
+                for (int i = 0; i < Songs.Length; i++)
+                {
+                    listboxSongs.Items.Add(Songs[i]); 
+                }
+
+            }
+
+            
+        }
+
+        private void listboxSongs_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //from listbox to player
+
+            WMPmusic.URL = paths[listboxSongs.SelectedIndex]; 
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
